1f054b56681f2a6dfae13e31eeee7135d2fd3cc9
Vinay Yadav <vnydav.0009@gmail.com>
2024-07-14 19:21:05 +0530

    Add Hamburger

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const left = document.querySelector(".left");
+let flag = true;
+hamburger.addEventListener("click", () => {
+  if(flag){
+    left.style.left = "0";
+    flag = false;
+  }
+    else{
+        left.style.left = "-100%";
+        flag = true;
+    }
+});
